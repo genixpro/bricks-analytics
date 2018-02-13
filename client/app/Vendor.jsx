9af984a886e65be71377c6a923421522026ce968
Bradley Arsenault <genixpro@gmail.com>
2018-02-13 18:25:56 -0500

    Added in initial code for the frontend web application.

---
+++ client/app/Vendor.jsx
@@ -0,0 +1,20 @@
+// Modernizr
+import './modernizr.js';
+// jQuery
+window.$ = require('jquery');
+// Local storage
+window.$.localStorage = require('js-storage').localStorage;
+// jQuery easing
+import '../node_modules/jquery.easing/jquery.easing.min.js';
+// Whirl
+import '../node_modules/whirl/dist/whirl.css';
+// Animo
+import '../node_modules/animo/animo.js';
+// Font Awesome
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+// Animate.CSS
+import '../node_modules/animate.css/animate.min.css';
+// Simple line icons
+import '../node_modules/simple-line-icons/css/simple-line-icons.css';
+// Localization
+import '../node_modules/jquery-localize/dist/jquery.localize.min.js';
